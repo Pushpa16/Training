382de68f31ec3b2440429ccf6aa6b81691a12e5e
Pushpa16 <96364372+Pushpa16@users.noreply.github.com>
2025-06-23 16:42:30 +0530

    Add files via upload

---
+++ Transcation/Transcation/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//example of transcation with single database
+namespace Transcation
+{  //example of normal transcation
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Before Transcation");
+    //        GetAccountDetails();
+
+    //        operation();
+
+    //        Console.WriteLine("After Transcation");
+    //        GetAccountDetails();
+
+
+    //    }
+    //    static void GetAccountDetails()
+    //    {
+    //        try
+    //        {
+    //            string ConString = "Server=WKSPUN05GTR1024;Database=BankDB;Trusted_Connection=true";
+    //            using (SqlConnection conn = new SqlConnection(ConString))
+    //            {
+    //                conn.Open();
+    //                SqlCommand cmd = new SqlCommand("Select * from Accounts", conn);
+    //                SqlDataReader rdr = cmd.ExecuteReader();
+    //                while (rdr.Read())
+    //                {
+    //                    Console.WriteLine(rdr["AccountNumber"] + " " + rdr["CustomerName"] + " " + rdr["Balance"]);
+    //                }
+    //            }
+
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //    }
+    //    static void operation()
+    //    {
+    //        try
+    //        {
+    //            string ConString = "Server=WKSPUN05GTR1024;Database=BankDB;Trusted_Connection=true";
+    //            using (SqlConnection conn = new SqlConnection(ConString))
+    //            {
+    //                conn.Open();
+    //                SqlTransaction trans= conn.BeginTransaction();
+    //                try
+    //                {
+    //                    SqlCommand cmd = new SqlCommand("update accounts set Balance=Balance-500 where AccountNumber='Account1'", conn, trans);
+    //                    cmd.ExecuteNonQuery();
+
+    //                    SqlCommand cmd1 = new SqlCommand("update accounts set Balance=Balance+500 where AccountNumber='Account2'", conn, trans);
+    //                    cmd1.ExecuteNonQuery();
+
+    //                    trans.Commit();
+    //                    Console.WriteLine("Successfully committed");
+    //                }
+    //                catch 
+    //                    {
+    //                        trans.Rollback();
+    //                        Console.WriteLine("Rollback due to error");
+    //                }
+    //            }
+
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //    }
+    //}
+
+
+    //example of verifying the data consistency
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before Transcation");
+            GetAccountDetails();
+
+            operation();
+
+            Console.WriteLine("After Transcation");
+            GetAccountDetails();
+
+
+        }
+        static void GetAccountDetails()
+        {
+            try
+            {
+                string ConString = "Server=WKSPUN05GTR1024;Database=BankDB;Trusted_Connection=true";
+                using (SqlConnection conn = new SqlConnection(ConString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("Select * from Accounts", conn);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine(rdr["AccountNumber"] + " " + rdr["CustomerName"] + " " + rdr["Balance"]);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void operation()
+        {
+            try
+            {
+                string ConString = "Server=WKSPUN05GTR1024;Database=BankDB;Trusted_Connection=true";
+                using (SqlConnection conn = new SqlConnection(ConString))
+                {
+                    conn.Open();
+                    SqlTransaction trans = conn.BeginTransaction();
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand("update accounts set Balance=Balance-500 where AccountNumber='Account1'", conn, trans);
+                        cmd.ExecuteNonQuery();
+
+                        SqlCommand cmd1 = new SqlCommand("update myaccounts set Balance=Balance+500 where AccountNumber='Account2'", conn, trans);
+                        cmd1.ExecuteNonQuery();
+
+                        trans.Commit();
+                        Console.WriteLine("Successfully committed");
+                    }
+                    catch
+                    {
+                        trans.Rollback();
+                        Console.WriteLine("Rollback due to error");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Transcation1/Transcation1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//example of transcation using distributed database
+//In order to create and use distributed transactions, first of all, we need to create an instance of the TransactionScope class. It is also
+//possible to open multiple database connections within the same transaction scope. The transaction scope will decide whether to create a local transaction or
+//a distributed transaction The transaction scope automatically converts a local transaction to a distributed transaction if required.
+
+namespace Transcation1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
