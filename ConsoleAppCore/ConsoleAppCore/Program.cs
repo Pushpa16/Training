24dcbd889bd1da686fce6e2b57c3aa64a57ca4b9
Pushpa16 <96364372+Pushpa16@users.noreply.github.com>
2025-06-23 16:49:21 +0530

    Add files via upload

---
+++ ConsoleAppCore/ConsoleAppCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
