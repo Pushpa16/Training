d2f62b131f6a7f4ff04b63b3ca2f146eff1e7a0e
Pushpa16 <96364372+Pushpa16@users.noreply.github.com>
2025-06-23 16:41:39 +0530

    Add files via upload

---
+++ Practice10/Practice10/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Practice10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Creating Customer Data Table
+                DataTable Cust = new DataTable("Customer");
+
+                // Adding Data Columns to the Customer Data Table
+                DataColumn CustomerId = new DataColumn("ID", typeof(Int32));
+                Cust.Columns.Add(CustomerId);
+                DataColumn CustomerName = new DataColumn("Name", typeof(string));
+                Cust.Columns.Add(CustomerName);
+                DataColumn CustomerMobile = new DataColumn("Mobile", typeof(string));
+                Cust.Columns.Add(CustomerMobile);
+
+                //Adding Data Rows into Customer Data Table
+                Cust.Rows.Add(101, "Anurag", "2233445566");
+                Cust.Rows.Add(202, "Manoj", "1234567890");
+
+
+                // Creating Orders Data Table
+                DataTable Ord = new DataTable("Orders");
+
+                // Adding Data Columns to the Orders Data Table
+                DataColumn OrderId = new DataColumn("ID", typeof(System.Int32));
+                Ord.Columns.Add(OrderId);
+                DataColumn CustId = new DataColumn("CustomerId", typeof(Int32));
+                Ord.Columns.Add(CustId);
+                DataColumn OrderAmount = new DataColumn("Amount", typeof(int));
+                Ord.Columns.Add(OrderAmount);
+
+                //Adding Data Rows into Orders Data Table
+                Ord.Rows.Add(10001, 101, 20000);
+                Ord.Rows.Add(10002, 102, 30000);
+
+
+                //Creating DataSet Object
+                DataSet ds = new DataSet();
+
+                //Adding DataTables into DataSet
+                ds.Tables.Add(Cust);
+                ds.Tables.Add(Ord);
+
+                Console.WriteLine("Details of Customer Table");
+                foreach (DataRow row in ds.Tables["Customer"].Rows )
+                {
+                    Console.WriteLine(row["ID"] + " "+ row["Name"]);
+                }
+
+                Console.WriteLine("Details of Order Table");
+                foreach (DataRow row1 in ds.Tables["Orders"].Rows)
+                {
+                    Console.WriteLine(row1["ID"] + " " + row1["CustomerId"]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + e);
+            }
+        }
+    
+    }
+}
---
+++ Practice11/Practice11/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice11
+{ //DataSet using SQL Server in C#
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string ConString = "Server=WKSPUN05GTR1024;Database=ShoppingCartDB;Trusted_Connection=true";
+                using (SqlConnection conn = new SqlConnection(ConString))
+                {
+                    //single select statement 
+                    //SqlDataAdapter da=new SqlDataAdapter("select * from customers", conn);
+                    //DataSet ds=new DataSet();
+
+                    //below: this will open the connection, execute the command,retrieve the result
+                    //       and will store the data in the dataset.we have not specified the data table
+                    //       name and the data table will be created at index position 0 and close the connection.
+                    //da.Fill(ds);
+
+                    //foreach (DataRow row in ds.Tables[0].Rows)
+                    //{
+                    //    Console.WriteLine(row["Id"] + ",  " + row["Name"] + ",  " + row["Mobile"]);
+                    // }
+
+
+                    //multiple select statement
+                    SqlDataAdapter da = new SqlDataAdapter("select * from customers; select * from orders", conn); 
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    Console.WriteLine("Customer table");
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        Console.WriteLine(row["Id"] + ",  " + row["Name"] + ",  " + row["Mobile"]);
+                    }
+
+                    Console.WriteLine("Order table");
+                    foreach (DataRow row in ds.Tables[1].Rows)
+                    {
+                        Console.WriteLine(row["Id"] + ",  " + row["CustomerID"]);
+                    }
+
+
+                    //Copy(): Copies both the structure and data of the DataSet.
+                    //Clone(): Copies the structure of the DataSet, including all schemas, relations, and constraints.no data
+                    //Clear(): Clears the DataSet of any data by removing all rows in all tables.
+
+
+                    // Remove a DataTable from a Dataset:
+                    Console.WriteLine();
+                    //Now, we want to delete the Orders data table from the DataSet
+                    if (ds.Tables.Contains("Orders") && ds.Tables.CanRemove(ds.Tables["Orders"]))
+                    {
+                        Console.WriteLine("Deleting Orders Data Table..");
+                        ds.Tables.Remove(ds.Tables["Orders"]);
+                        //dataSet.Tables.Remove(dataSet.Tables[1]);
+                    }
+                    //Now check whether the DataTable exists or not
+                    if (ds.Tables.Contains("Orders"))
+                    {
+                        Console.WriteLine("Orders Data Table Exits");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Orders Data Table Not Exits Anymore");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Occurred: {ex.Message}");
+            }
+
+        }
+    }
+}
